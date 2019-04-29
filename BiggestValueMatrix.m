8d2ea26d004342f6aa0e0046e04799b7a012f8b6
Max Base <MaxBaseCode@Gmail.Com>
2019-04-29 17:38:10 +0430

    Update BiggestValueMatrix.m

--- BiggestValueMatrix.m
+++ BiggestValueMatrix.m
@@ -3,3 +3,21 @@
 % Programmer : Max
 % Date : 2019-04-29
 %
+
+function [value, row_max, column_max] = Biggest(Matrix)
+	maximum=Matrix(1,1);
+	[rows,columns] = size(ndata);
+	for i=1:rows
+		for j=1:columns
+			if maximum <= Matrix(i,j);
+				maximum=Matrix(i,j);
+			end
+		end
+	end
+	[row_max column_max] = find(A == maximum);
+	return
+end
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+Matrix=[99 67 65;
+	63 62 61;
+	41 40 9];
